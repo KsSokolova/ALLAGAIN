bd4e25aeaad6f01fdd524b082f0ed7c85c4e7df4
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-22 21:59:31 +0300

    Task 3

--- task0Seminar1/Program.cs
+++ task0Seminar1/Program.cs
@@ -5,9 +5,21 @@
 // Console.WriteLine (square);
 
 // Задача 2. Напишите программу, кот на вход принимает 2 числа и проверяет, является ли первое число квадратом второго
-Console.WriteLine ("Введите число А: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Введите число B: ");
-int b = Convert.ToInt32(Console.ReadLine());
-if (a == b*b) Console.WriteLine ($"da");
-else Console.WriteLine ($"net");
+// Console.WriteLine ("Введите число А: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите число B: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a == b*b) Console.WriteLine ($"da");
+// else Console.WriteLine ($"net");
+
+
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру
+Console.WriteLine ("Напишите цифру дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1) Console.WriteLine ($"Это понедельник");
+if (number == 2) Console.WriteLine ($"Это вторник"); 
+if (number == 3) Console.WriteLine ($"Это среда");
+if (number == 4) Console.WriteLine ($"Это чт");
+if (number == 5) Console.WriteLine ($"Это пт");
+if (number == 6) Console.WriteLine ($"Это сб");
+if (number == 7) Console.WriteLine ($"Это вск");

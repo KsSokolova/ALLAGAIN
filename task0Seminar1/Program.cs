bb3722b45fa91b18ce3274740a6f87ec3f46eec3
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-22 21:43:03 +0300

    Added task 2

--- task0Seminar1/Program.cs
+++ task0Seminar1/Program.cs
@@ -1,5 +1,13 @@
 ﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
-Console.Write ("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int square = number*number;
-Console.WriteLine (square);
+// Console.Write ("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int square = number*number;
+// Console.WriteLine (square);
+
+// Задача 2. Напишите программу, кот на вход принимает 2 числа и проверяет, является ли первое число квадратом второго
+Console.WriteLine ("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b) Console.WriteLine ($"da");
+else Console.WriteLine ($"net");

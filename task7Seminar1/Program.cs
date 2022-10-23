7ed8b8633d6f7477f6e0ea2d6cf69317510ba8e5
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-23 06:45:00 +0300

    Task 4

--- task7Seminar1/Program.cs
+++ task7Seminar1/Program.cs
@@ -14,4 +14,14 @@
 // if (number2>number1) max = number2;
 // Console.WriteLine($"max {max}");
 
-
+//Задача 4. Напишите программу, которая на вход принимает 3 числа и выдает максимальное
+// Console.WriteLine("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// int max = number1;
+// if (number2>number1) max = number2;
+// if (number3>number2) max = number3;
+// Console.WriteLine($"max {max}");

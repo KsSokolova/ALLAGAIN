3394f69bdd6297f9e5912d56880ebbc48803866d
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-23 06:26:17 +0300

    Task 7

---
+++ task7Seminar1/Program.cs
@@ -0,0 +1,6 @@
+﻿//7. Создайте программу, которая на вход принимает трехзначное число, а на выходе показывает третью цифру этого числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = 0;
+thirdDigit = number%10;
+Console.WriteLine (thirdDigit);

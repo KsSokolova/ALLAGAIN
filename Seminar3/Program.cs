fd48f8e4d1193fd17cf3f0909d58ccdeb64a1c48
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-23 12:15:20 +0300

    Task 18

---
+++ Seminar3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 18. НАпишите программу, которая по заданной четверти выдает диапазон возможных координат в этой четверти
+Console.WriteLine ("Задайте четверть 1, 2, 3 или 4: ");
+int sqr = Convert.ToInt32(Console.ReadLine());
+if (sqr == 1) Console.WriteLine ($"X>0 & y>0");
+else if (sqr == 2) Console.WriteLine ($"X>0 & y<0"); 
+else if (sqr == 3) Console.WriteLine ($"X<0 & y<0"); 
+else if (sqr == 4) Console.WriteLine ($"X>0 & y>0"); 

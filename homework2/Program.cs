e491800a2a9bcdc8d538f1741385898e2ee88d72
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-23 09:23:42 +0300

    Task 13

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -6,3 +6,15 @@
 // int y = x%10;
 // Console.WriteLine(y);
 
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5     78 -> третьей цифры нет      32679 -> 6
+
+Console.WriteLine("Задача 13. Введите число: ");
+string? test = Console.ReadLine();
+if (test.Length<3){
+    Console.WriteLine($"Третьей цифры нет");
+}
+else 
+{
+Console.WriteLine($"{test[2]}");
+}

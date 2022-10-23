74993ec62d4dda6b48e672d5acd9adbc9e6ee32d
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-23 06:39:29 +0300

    Task 2

--- task7Seminar1/Program.cs
+++ task7Seminar1/Program.cs
@@ -1,6 +1,17 @@
 ﻿//7. Создайте программу, которая на вход принимает трехзначное число, а на выходе показывает третью цифру этого числа
-Console.WriteLine("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int thirdDigit = 0;
-thirdDigit = number%10;
-Console.WriteLine (thirdDigit);
+// Console.WriteLine("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int thirdDigit = 0;
+// thirdDigit = number%10;
+// Console.WriteLine (thirdDigit);
+
+// Задача 2. Напишите программу, которая на вход принимает 2 числа и выдает, какое больше 
+// Console.WriteLine("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int max = number1;
+// if (number2>number1) max = number2;
+// Console.WriteLine($"max {max}");
+
+

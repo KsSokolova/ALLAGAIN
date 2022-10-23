faa4c13d9aea31181c7e48e2e54ce0eacc81fbc4
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-23 09:42:11 +0300

    Task 15

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -9,12 +9,18 @@
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5     78 -> третьей цифры нет      32679 -> 6
 
-Console.WriteLine("Задача 13. Введите число: ");
-string? test = Console.ReadLine();
-if (test.Length<3){
-    Console.WriteLine($"Третьей цифры нет");
-}
-else 
-{
-Console.WriteLine($"{test[2]}");
-}
+// Console.WriteLine("Задача 13. Введите число: ");
+// string? test = Console.ReadLine();
+// if (test.Length<3){
+//     Console.WriteLine($"Третьей цифры нет");
+// }
+// else 
+// {
+// Console.WriteLine($"{test[2]}");
+// }
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да   7 -> да   1 -> нет
+Console.WriteLine("Задача 15. Введите цифру дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>=6) Console.WriteLine($"da");
+else Console.WriteLine($"net");

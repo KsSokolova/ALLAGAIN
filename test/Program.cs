c872b2f2ec97956f85f851a38f3033cf85e0ba40
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-24 10:55:23 +0300

    Task 19

--- test/Program.cs
+++ test/Program.cs
@@ -1,6 +1,14 @@
-﻿int end = 10;
+﻿// Программа, кот выдает четные числа без проверки на четность
+// int end = 10;
 
-for (int start = 1; start < end; start+=2)
-{
-    Console.WriteLine(start);
-}
+// for (int start = 1; start < end; start+=2)
+// {
+//     Console.WriteLine(start);
+// }
+
+// Задача 19. Напишите программу, кот принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+// 14212 - нет    23432 да
+Console.WriteLine("Введите пятизначное число: ");
+string test = Console.ReadLine();
+if ((test[0]==test[4]) && (test[1]==test[3])) Console.WriteLine ($"да");
+else Console.WriteLine ($"нет");

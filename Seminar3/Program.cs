c63c8637cd46d8ac90b35be46512fddf4bb75a7a
Ksenia Sokolova <forsokolova@gmail.com>
2022-10-24 09:12:43 +0300

    Task 21

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,7 +1,22 @@
-﻿// Задача 18. НАпишите программу, которая по заданной четверти выдает диапазон возможных координат в этой четверти
-Console.WriteLine ("Задайте четверть 1, 2, 3 или 4: ");
-int sqr = Convert.ToInt32(Console.ReadLine());
-if (sqr == 1) Console.WriteLine ($"X>0 & y>0");
-else if (sqr == 2) Console.WriteLine ($"X>0 & y<0"); 
-else if (sqr == 3) Console.WriteLine ($"X<0 & y<0"); 
-else if (sqr == 4) Console.WriteLine ($"X>0 & y>0"); 
+﻿// // Задача 18. НАпишите программу, которая по заданной четверти выдает диапазон возможных координат в этой четверти
+// Console.WriteLine ("Задайте четверть 1, 2, 3 или 4: ");
+// int sqr = Convert.ToInt32(Console.ReadLine());
+// if (sqr == 1) Console.WriteLine ($"X>0 & y>0");
+// else if (sqr == 2) Console.WriteLine ($"X>0 & y<0"); 
+// else if (sqr == 3) Console.WriteLine ($"X<0 & y<0"); 
+// else if (sqr == 4) Console.WriteLine ($"X>0 & y>0"); 
+
+// Задача 21. Напишите программу, кот. прнимает на вход координаты 2 точек и находит расстояние между ними в 2d пространстве
+// d = √ (х2-х1)*2 + (y2-y1)*2
+// Console.WriteLine("Введите x1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите x2: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите y2: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// double d = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2));
+// Console.WriteLine (Math.Round(d,3));
+
+
